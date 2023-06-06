a0788b8895c7b91af668cd5eba4037f9937afbe7
Ahmed Taleb Elemine <labsar.ahmed@icloud.com>
2023-06-05 21:17:35 -0500

    Created Model POJO classes Message and Account

--- spring-boot-application-run/src/main/java/Model/Account.java
+++ spring-boot-application-run/src/main/java/Model/Account.java
@@ -1,4 +1,4 @@
-package com.javatpoint.springbootapplicationrun;
+package Model;
 
 
 
--- spring-boot-application-run/src/main/java/com/javatpoint/springbootapplicationrun/GreetingController.java
+++ spring-boot-application-run/src/main/java/com/javatpoint/springbootapplicationrun/GreetingController.java
@@ -1,6 +1,9 @@
 package com.javatpoint.springbootapplicationrun;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RestController;
+
+import Model.Account;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
